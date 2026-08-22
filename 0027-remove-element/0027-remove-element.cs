3d60875c082e0f238eae635e2cb1b5f44a0087aa
Jai Gupta <81135161+jai-gupta-dev@users.noreply.github.com>
2026-08-22 20:20:50 +0530

    Time: 0 ms (100%), Space: 46.6 MB (74.77%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int[] a = new int[nums.Length];
+        int count =0;
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
